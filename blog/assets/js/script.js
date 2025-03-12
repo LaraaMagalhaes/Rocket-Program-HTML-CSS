81ed55750e991d4ec5d6ccd0df6d65eaeb23db0d
LaraaMagalhaes <annalarammv123@gmail.com>
2025-03-12 13:08:05 -0300

    .

---
+++ blog/assets/js/script.js
@@ -0,0 +1,11 @@
+const menuEl = document.getElementById("menu");
+const linksEls = document.getElementsByTagName("a");
+const postsEls = document.getElementsByClassName("post");
+const headerButtonEl = document.querySelector('#header button');
+const paragraphsEl = document.querySelector('.post p');
+
+console.dir(menuEl);
+console.log(linksEls)
+console.log(postsEls)
+console.dir(headerButtonEl);
+console.dir(paragraphsEl);
